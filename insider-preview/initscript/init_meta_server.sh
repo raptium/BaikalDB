802d3c895c2e81d9001ec20cc4a0af85ff0ab8ed
liutao04 <liutao04@baidu.com>
2020-11-16 17:35:16 +0800

    adjust meta idc

--- insider-preview/initscript/init_meta_server.sh
+++ insider-preview/initscript/init_meta_server.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-#Created on 2017-11-22 
-#测试场景：完成的meta_server流程
+#验证场景：完成的meta_server流程
 
 echo -e "测试场景：添加逻辑机房bj nj hz\n"
 curl -d '{
     "op_type": "OP_ADD_LOGICAL",
     "logical_rooms": {
-        "logical_rooms" : ["bj", "nj", "hz", "yq", "gz"]
+        "logical_rooms" : ["bj", "nj", "gz"]
     }
 }' http://$1/MetaService/meta_manager
 echo -e "\n"
@@ -17,7 +16,7 @@ curl -d '{
     "op_type": "OP_ADD_PHYSICAL",
     "physical_rooms": {
         "logical_room" : "bj",
-        "physical_rooms" : ["tc", "yf01", "m1", "bjyz", "cq01", "cq02", "cp01", "dbl01", "st01", "bjhw"]
+        "physical_rooms" : ["bj"]
     }
 }' http://$1/MetaService/meta_manager
 echo -e "\n"
@@ -26,39 +25,21 @@ curl -d '{
     "op_type": "OP_ADD_PHYSICAL",
     "physical_rooms": {
         "logical_room" : "nj",
-        "physical_rooms" : ["nj01", "nj02", "nj03", "njjs"]
-    }
-}' http://$1/MetaService/meta_manager
-
-echo -e "\n"
-curl -d '{
-    "op_type": "OP_ADD_PHYSICAL",
-    "physical_rooms": {
-        "logical_room" : "hz",
-        "physical_rooms" : ["hz01"]
+        "physical_rooms" : ["nj"]
     }
 }' http://$1/MetaService/meta_manager
 echo -e "\n"
 
-echo -e "\n"
 curl -d '{
     "op_type": "OP_ADD_PHYSICAL",
     "physical_rooms": {
         "logical_room" : "gz",
-        "physical_rooms" : ["gzbh", "gzhl", "gzns", "gzhxy"]
+        "physical_rooms" : ["gz"]
     }
 }' http://$1/MetaService/meta_manager
 echo -e "\n"
 
-echo -e "\n"
-curl -d '{
-    "op_type": "OP_ADD_PHYSICAL",
-    "physical_rooms": {
-        "logical_room" : "yq",
-        "physical_rooms" : ["yq01"]
-    }
-}' http://$1/MetaService/meta_manager
-echo -e "\n"
+
 #查询逻辑机房
 echo -e "查询逻辑机房\n"
 curl -d '{
